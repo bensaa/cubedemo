82c2faf8aadfeb08b35a42bb6f170221869716fc
bensaa <b.saamaki@gmail.com>
2021-05-10 18:12:55 +0300

    indetation modifications

--- cubedemo.cpp
+++ cubedemo.cpp
@@ -14,36 +14,36 @@ int fps = 0;
 bool gamerunning = true;
 
 bool comparecubes(cube* cp1, cube* cp2) {
-    return (cp1->locz > cp2->locz);
+	return (cp1->locz > cp2->locz);
 }
 static void initplaces(){
-    for(int i = 0; i < CUBECOUNT;i++){
-        for(int i2 = 0; i2 < 8;i2++){
-            point* P = &cubelist[i].points[i2];
-            P->dx = cubelist[i].locx + P->sx;
-            P->dy = cubelist[i].locy + P->sy;
-            P->dz = cubelist[i].locz + P->sz;
-        }
-    }
+	for(int i = 0; i < CUBECOUNT;i++){
+		for(int i2 = 0; i2 < 8;i2++){
+			point* P = &cubelist[i].points[i2];
+			P->dx = cubelist[i].locx + P->sx;
+			P->dy = cubelist[i].locy + P->sy;
+			P->dz = cubelist[i].locz + P->sz;
+		}
+	}
 }
 static void calcplace(){
 
-    for(int i = CUBECOUNT-1; i >= 0;i--){
-        int i2 = 0;
-        for(; i2 < 8;i2++){
-            point* P = &order[i]->points[i2];
-            P->fz = P->dz - cameraz;
+	for(int i = CUBECOUNT-1; i >= 0;i--){
+		int i2 = 0;
+		for(; i2 < 8;i2++){
+			point* P = &order[i]->points[i2];
+			P->fz = P->dz - cameraz;
 
-            if(P->fz < -400){
-                order[i]->locz = order[i]->locz + 4000;
-                order[i]->locx = rand()%1000 -500;
-                order[i]->locy = rand()%1000 -500 ;
-                for(int i3 = 0; i3 < 8;i3++){
-                    point* P2 = &order[i]->points[i3];
-                    P2->dx = order[i]->locx + P2->sx;
-                    P2->dy = order[i]->locy + P2->sy;
-                    P2->dz = order[i]->locz + P2->sz;
-		        }
+			if(P->fz < -400){
+				order[i]->locz = order[i]->locz + 4000;
+				order[i]->locx = rand()%1000 -500;
+				order[i]->locy = rand()%1000 -500 ;
+				for(int i3 = 0; i3 < 8;i3++){
+					point* P2 = &order[i]->points[i3];
+					P2->dx = order[i]->locx + P2->sx;
+					P2->dy = order[i]->locy + P2->sy;
+					P2->dz = order[i]->locz + P2->sz;
+				}
 
 				cube* tmp_cube = order[i];
 
@@ -51,28 +51,28 @@ static void calcplace(){
 					order[i3] = order[i3-1];
 				}
 				order[0] = tmp_cube;
-                i = CUBECOUNT;
+				i = CUBECOUNT;
 
 				break;
-            }
+			}
 			else{
 				P->fx = P->dx - camerax;
 				P->fy = P->dy - cameray;
 				P->fx = HscreenW + P->fx * 600 /((P->fz + 600));
 				P->fy = HscreenH - P->fy * 600 /((P->fz + 600));
 			}
-        }
-    }
+		}
+	}
 }
 
 class Window3d : public Gtk::DrawingArea{
 public:
-    Window3d(){
-        Glib::signal_timeout().connect( sigc::mem_fun(*this, &Window3d::on_timeout), 1000 );
-    };
-    bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override{
-        cr->set_line_width(1.0);
-        for(int j = 0; j < CUBECOUNT;j++){
+	Window3d(){
+		Glib::signal_timeout().connect( sigc::mem_fun(*this, &Window3d::on_timeout), 1000 );
+	};
+	bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override{
+		cr->set_line_width(1.0);
+		for(int j = 0; j < CUBECOUNT;j++){
 			for(int i = 0; i < 12;i++){
 				kolmio* KP = &(order[j]->kolmiot[i]);
 				float P1x = KP->points[2]->fx - KP->points[0]->fx;
@@ -90,41 +90,41 @@ public:
 					cr->set_source_rgb(0.0, 0.0, 0.0);
 					cr->stroke();
 				}
-            }
-        }
+			}
+		}
 
-        std::string s = ("FPS: " + std::to_string(fps));
-        auto layout = create_pango_layout(s);
-        cr->move_to(25, 25);
-        layout->show_in_cairo_context(cr);
+		std::string s = ("FPS: " + std::to_string(fps));
+		auto layout = create_pango_layout(s);
+		cr->move_to(25, 25);
+		layout->show_in_cairo_context(cr);
 
-        return true;
-    };
-    bool on_timeout(){
-        fps = iterations;
-        iterations = 0;
-        return true;
-    };
+		return true;
+	};
+	bool on_timeout(){
+		fps = iterations;
+		iterations = 0;
+		return true;
+	};
 };
 Window3d* Hwin = NULL;
 
 static void onResize(){
-    HscreenW = Hwin->get_width()/2;
-    HscreenH = Hwin->get_height()/2;
+	HscreenW = Hwin->get_width()/2;
+	HscreenH = Hwin->get_height()/2;
 }
 static void gameloop(){
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	while(gamerunning){
-        cameraz = cameraz + 1;
-	    calcplace();
-        Hwin->queue_draw();
-        iterations++;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    }
+		cameraz = cameraz + 1;
+		calcplace();
+		Hwin->queue_draw();
+		iterations++;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
 }
 
 static bool onKeyPress(GdkEventKey* event){
-    switch(event->hardware_keycode){
+	switch(event->hardware_keycode){
 		case 24:{
 			gamerunning = false;
 			((Gtk::Application*)Happ)->quit();
@@ -136,30 +136,30 @@ static bool onKeyPress(GdkEventKey* event){
 			break;
 		}
 	}
-    return true;
+	return true;
 }
 
 int main(int argc, char** argv){
-    for(int i = 0;i < CUBECOUNT;i++){
-        createcube(&cubelist[i],100.0f);
-        order[i] = &cubelist[i];
-        cubelist[i].locx = rand() % 1200 -600;
-        cubelist[i].locy = rand() % 1200 -600;
-        cubelist[i].locz = rand() % 3800 +200;
-    }
-    initplaces();
+	for(int i = 0;i < CUBECOUNT;i++){
+		createcube(&cubelist[i],100.0f);
+		order[i] = &cubelist[i];
+		cubelist[i].locx = rand() % 1200 -600;
+		cubelist[i].locy = rand() % 1200 -600;
+		cubelist[i].locz = rand() % 3800 +200;
+	}
+	initplaces();
    	std::sort(&order[0],&order[CUBECOUNT],comparecubes);
 	auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-    Happ = &app;
-    Gtk::Window win;
-    win.set_title("Window3d");
-    Window3d drawarea_3D;
-    win.fullscreen();
-    Hwin = &drawarea_3D;
-    win.signal_key_press_event().connect(sigc::ptr_fun(onKeyPress),false );
-    win.signal_check_resize().connect(sigc::ptr_fun(onResize),false );
-    win.add(drawarea_3D);
-    drawarea_3D.show();
-    std::thread t(gameloop);
-    return app->run(win);
+	Happ = &app;
+	Gtk::Window win;
+	win.set_title("Window3d");
+	Window3d drawarea_3D;
+	win.fullscreen();
+	Hwin = &drawarea_3D;
+	win.signal_key_press_event().connect(sigc::ptr_fun(onKeyPress),false );
+	win.signal_check_resize().connect(sigc::ptr_fun(onResize),false );
+	win.add(drawarea_3D);
+	drawarea_3D.show();
+	std::thread t(gameloop);
+	return app->run(win);
 }
--- elements.h
+++ elements.h
@@ -15,40 +15,40 @@
 #include <thread>
 
 struct point{
-    float sx,sy,sz;
-    float dx,dy,dz;
-    float fx,fy,fz;
+	float sx,sy,sz;
+	float dx,dy,dz;
+	float fx,fy,fz;
 };
 struct kolmio{
-    point* points[3];
+	point* points[3];
 };
 struct cube{
-    kolmio kolmiot[12];
-    point points[8];
-    float locx,locy,locz;
+	kolmio kolmiot[12];
+	point points[8];
+	float locx,locy,locz;
 };
 
 static void createcube(cube* E, float radius){
-    E->points[0] = {.sx = radius,.sy = radius,.sz = -radius};
-    E->points[1] = {.sx =-radius,.sy = radius,.sz = -radius};
-    E->points[2] = {.sx =-radius,.sy =-radius,.sz = -radius};
-    E->points[3] = {.sx = radius,.sy =-radius,.sz = -radius};
-    E->points[4] = {.sx = radius,.sy = radius,.sz =  radius};
-    E->points[5] = {.sx =-radius,.sy = radius,.sz =  radius};
-    E->points[6] = {.sx =-radius,.sy =-radius,.sz =  radius};
-    E->points[7] = {.sx = radius,.sy =-radius,.sz =  radius};
-    E->kolmiot[0] = {.points{&E->points[0],&E->points[1],&E->points[2]}};//front1
-    E->kolmiot[1] = {.points{&E->points[0],&E->points[2],&E->points[3]}};//front2
-    E->kolmiot[2] = {.points{&E->points[1],&E->points[5],&E->points[6]}};//left1
-    E->kolmiot[3] = {.points{&E->points[1],&E->points[6],&E->points[2]}};//left2
-    E->kolmiot[4] = {.points{&E->points[0],&E->points[4],&E->points[5]}};//roof1
-    E->kolmiot[5] = {.points{&E->points[0],&E->points[5],&E->points[1]}};//roof2
-    E->kolmiot[6] = {.points{&E->points[0],&E->points[7],&E->points[4]}};//right1
-    E->kolmiot[7] = {.points{&E->points[0],&E->points[3],&E->points[7]}};//right2
-    E->kolmiot[8] = {.points{&E->points[4],&E->points[6],&E->points[5]}};//back1
-    E->kolmiot[9] = {.points{&E->points[4],&E->points[7],&E->points[6]}};//back2
-    E->kolmiot[10] = {.points{&E->points[3],&E->points[6],&E->points[7]}};//floor1
-    E->kolmiot[11] = {.points{&E->points[3],&E->points[2],&E->points[6]}};//floor2
+	E->points[0] = {.sx = radius,.sy = radius,.sz = -radius};
+	E->points[1] = {.sx =-radius,.sy = radius,.sz = -radius};
+	E->points[2] = {.sx =-radius,.sy =-radius,.sz = -radius};
+	E->points[3] = {.sx = radius,.sy =-radius,.sz = -radius};
+	E->points[4] = {.sx = radius,.sy = radius,.sz =  radius};
+	E->points[5] = {.sx =-radius,.sy = radius,.sz =  radius};
+	E->points[6] = {.sx =-radius,.sy =-radius,.sz =  radius};
+	E->points[7] = {.sx = radius,.sy =-radius,.sz =  radius};
+	E->kolmiot[0] = {.points{&E->points[0],&E->points[1],&E->points[2]}};//front1
+	E->kolmiot[1] = {.points{&E->points[0],&E->points[2],&E->points[3]}};//front2
+	E->kolmiot[2] = {.points{&E->points[1],&E->points[5],&E->points[6]}};//left1
+	E->kolmiot[3] = {.points{&E->points[1],&E->points[6],&E->points[2]}};//left2
+	E->kolmiot[4] = {.points{&E->points[0],&E->points[4],&E->points[5]}};//roof1
+	E->kolmiot[5] = {.points{&E->points[0],&E->points[5],&E->points[1]}};//roof2
+	E->kolmiot[6] = {.points{&E->points[0],&E->points[7],&E->points[4]}};//right1
+	E->kolmiot[7] = {.points{&E->points[0],&E->points[3],&E->points[7]}};//right2
+	E->kolmiot[8] = {.points{&E->points[4],&E->points[6],&E->points[5]}};//back1
+	E->kolmiot[9] = {.points{&E->points[4],&E->points[7],&E->points[6]}};//back2
+	E->kolmiot[10] = {.points{&E->points[3],&E->points[6],&E->points[7]}};//floor1
+	E->kolmiot[11] = {.points{&E->points[3],&E->points[2],&E->points[6]}};//floor2
 };
 
 #endif
