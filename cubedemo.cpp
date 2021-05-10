b190fb6c56f3f07cb125a2ebc0ea63245660b6c8
bensaa <b.saamaki@gmail.com>
2021-05-10 17:49:35 +0300

    indentation fixed

--- cubedemo.cpp
+++ cubedemo.cpp
@@ -125,12 +125,15 @@ static void gameloop(){
 
 static bool onKeyPress(GdkEventKey* event){
     switch(event->hardware_keycode){
-	case 24:{                                                                                        gamerunning = false;
-            ((Gtk::Application*)Happ)->quit();
-            break;                                                                                   }
-	case 9:{                                                                                         gamerunning = false;
-            ((Gtk::Application*)Happ)->quit();
-            break;                                                                                   }
+	case 24:{
+		gamerunning = false;
+		((Gtk::Application*)Happ)->quit();
+		break;
+	}
+	case 9:{
+		gamerunning = false;
+		((Gtk::Application*)Happ)->quit();
+		break;                                                                                   }
 	}
     return true;
 }
